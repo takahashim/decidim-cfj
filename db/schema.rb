b5dd0d946a122bc86441f29dd3cc34fee318e41f
takahashim <takahashimm@gmail.com>
2024-11-18 20:35:22 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_13_180919) do
+ActiveRecord::Schema.define(version: 2024_07_29_104240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1316,8 +1316,8 @@ ActiveRecord::Schema.define(version: 2024_07_13_180919) do
     t.jsonb "body"
     t.integer "comments_count", default: 0, null: false
     t.integer "follows_count", default: 0, null: false
-    t.integer "state", default: 0, null: false
     t.integer "valuation_assignments_count", default: 0
+    t.integer "state", default: 0, null: false
     t.index "md5((body)::text)", name: "decidim_proposals_proposal_body_search"
     t.index "md5((title)::text)", name: "decidim_proposals_proposal_title_search"
     t.index ["created_at"], name: "index_decidim_proposals_proposals_on_created_at"
